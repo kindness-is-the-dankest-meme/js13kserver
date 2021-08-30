951dae7d7d5f65622bf528723d9d64056b7c165c
Matt Hayes <matt@mysterycommand.com>
2021-08-30 01:14:49 -0500

    feat: add socket-based negotiation logic, it works!

--- public/client.js
+++ public/client.js
@@ -35,7 +35,82 @@ dispatch(win, 'resize');
 /**
  * socket
  */
-io({ upgrade: false, transports: ['websocket'] });
+const socket = io({ upgrade: false, transports: ['websocket'] });
+const connection = new RTCPeerConnection({
+  iceServers: [
+    {
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun3.l.google.com:19302',
+      ],
+    },
+  ],
+});
+const channel = connection.createDataChannel('@kitdm/js13kgames-2021');
+
+const isRude = 'ontouchstart' in win;
+if (isRude) {
+  subscribe(connection, 'negotiationneeded', async () => {
+    try {
+      const offer = await connection.createOffer();
+      await connection.setLocalDescription(new RTCSessionDescription(offer));
+      socket.send(JSON.stringify({ description: connection.localDescription }));
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+
+subscribe(connection, 'icecandidate', ({ candidate }) =>
+  socket.send(JSON.stringify({ candidate })),
+);
+
+socket.on('message', async (data) => {
+  const { candidate, description } = JSON.parse(data);
+
+  try {
+    if (candidate && candidate.candidate !== '') {
+      await connection.addIceCandidate(candidate);
+    }
+
+    if (description) {
+      /**
+       * @see https://github.com/node-webrtc/node-webrtc/issues/674
+       * @see https://github.com/node-webrtc/node-webrtc/issues/677
+       */
+      await connection.setRemoteDescription(description);
+
+      if (!isRude && description.type === 'offer') {
+        const answer = await connection.createAnswer();
+        await connection.setLocalDescription(answer);
+
+        socket.send(
+          JSON.stringify({ description: connection.localDescription }),
+        );
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+subscribe(channel, 'open', (event) => {
+  console.log('open', event);
+});
+
+subscribe(channel, 'close', (event) => {
+  console.log('close', event);
+});
+
+subscribe(channel, 'error', (event) => {
+  console.log('error', event);
+});
+
+subscribe(channel, 'message', (event) => {
+  console.log('message', event);
+});
 
 /**
  * state
--- public/server.js
+++ public/server.js
@@ -1,5 +1,21 @@
+const sockets = new Map();
+
 module.exports = {
   io: (socket) => {
-    console.log('Connected: ' + socket.id);
+    sockets.set(socket.id, socket);
+
+    socket.on('message', (data) => {
+      sockets.forEach((s, i) => {
+        if (socket.id === i) {
+          return;
+        }
+
+        s.send(data);
+      });
+    });
+
+    socket.on('disconnect', () => {
+      sockets.delete(socket.id);
+    });
   },
 };
