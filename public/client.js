6eae34f9c63039dfd372a97c206a214f26e3a381
Matt Hayes <matt@mysterycommand.com>
2021-08-29 22:36:42 -0500

    feat: styles, canvas id, resizing and some event stuff

--- public/client.js
+++ public/client.js
@@ -1,1 +1,24 @@
+const W = window;
+const { devicePixelRatio: dpr } = W;
+
+const subscribe = (target, type, listener, options = false) => {
+  target.addEventListener(type, listener, options);
+  return () => target.removeEventListener(type, listener, options);
+};
+
+const dispatch = (target, type) => target.dispatchEvent(new Event(type));
+
+subscribe(W, 'resize', () => {
+  const { innerWidth: w, innerHeight: h } = W;
+
+  c.width = w * dpr;
+  c.height = h * dpr;
+
+  c.style.width = `${c.width}px`;
+  c.style.height = `${c.height}px`;
+  c.style.transform = `scale(${1 / dpr})`;
+});
+
+dispatch(W, 'resize');
+
 io({ upgrade: false, transports: ['websocket'] });
