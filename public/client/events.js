ff4adafbf6d56f04d6bcfd4fb7fd3b45a76ae201
Matt Hayes <matt@mysterycommand.com>
2021-09-11 20:57:09 -0500

    refactor: the conciseness of this is cool

--- public/client/events.js
+++ public/client/events.js
@@ -1,9 +1,8 @@
 /**
  * events
  */
-export const subscribe = (target, type, listener, options = false) => {
-  target.addEventListener(type, listener, options);
-  return () => target.removeEventListener(type, listener, options);
-};
+export const subscribe = (target, type, listener, options = false) =>
+  target.addEventListener(type, listener, options) ||
+  (() => target.removeEventListener(type, listener, options));
 
 export const dispatch = (target, type) => target.dispatchEvent(new Event(type));
